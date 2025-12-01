7412a0f6e9e88dcf0ab0e71f19ab3f9f8aa5cd55
arinakonst <arina.i.konst@bk.ru>
2025-12-01 18:50:38 +0300

    Update яндекс_вариант_8.py

--- яндекс_вариант_8.py
+++ яндекс_вариант_8.py
@@ -5,29 +5,17 @@ def fun(s):
     for x in s:
         if x in 'гпрбл':
             s2 += '1'
-        else:
+        elif x in 'иеоа':
             s2 += '0'
-    if s[0] != '0' and s[-1] != '0' and '101' not in s:
+    if s2[0] != '0' and s2[-1] != '0' and '101' not in s2:
         return 1
     else:
         return 0
     
-word =''
 k = 0
-for a in 'гипербола':
-    word += a
-    for b in 'гипербола':
-        word += b
-        for c in 'гипербола':
-            word += c
-            for d in 'гипербола':
-                word += d
-                for e in 'гипербола':
-                    word += e
-                    for f in 'гипербола':
-                        word += f
-                        if fun(word) == 1:
-                            k += 1
-                        word = ''
+a = list(product('гипербола', repeat = 6))
+for x in a:
+    if fun(x) == 1:
+        k += 1
 
-print(k)    #531441
+print(k)    #68025
